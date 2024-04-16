03d4d4d24eacdcadd23fe0aef7e229b3dc098256
DOzajec <167138616+DOzajec@users.noreply.github.com>
2024-04-16 07:15:04 +0200

    zad_tut

---
+++ komendy.sh
@@ -0,0 +1,76 @@
+git commit
+git branch
+git checkout
+git merge
+git rebase
+git checkout C4
+git checkout HEAD^
+git branch -f 
+git reset HEAD~1
+git revert HEAD
+git cherry-pick C3 C4 C7
+git rebase -i HEAD~4
+git checkout main
+git cherr-pick C4
+git rebase —i HEAD~2
+git rebase -i HEAD~1
+git rebase —i HEAD~2
+git branch -f main HEAD
+git checkout main
+git cherry-pick C2
+git checkout C1
+git branch-f main HEAD
+git cherry-pick C2'
+git cherry-pick C3
+git branch -f main HEAD
+git tag v0 C1
+git tag v1 C2
+git checkout C2
+git describe main
+git describe side
+git describe bugFix
+git commit
+git checkout bugFix
+git rebase -i main
+git branch -f main HEAD
+git checkout side
+git rebase -i main
+git branch - f main HEAD
+git checkout another
+git rebase -i main
+git branch -f main HEAD
+git checkout HEAD~1^2~1
+git branch bugWork
+git checkout main
+git checkout C1
+git cherry-pick C4 C3 C2
+git branch -f one HEAD
+git checkout C1
+git cherry-pick C5 C4' C3'
+git branch -f two HEAD
+git checkout C 2
+git branch -f three HEAD
+git clone
+git commit
+git checout C1
+git commit
+git fetch
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
